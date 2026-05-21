dbe57c549990e82dd3a1347219c9daf17502b088
Ivan Lylin <ivanlylin2009@gmail.com>
2026-05-21 09:54:22 +0300

    fix

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,14 @@
+local function toggle_terminal()
+    Snacks.terminal()
+end
+
+local function toggle_terminal_from_terminal()
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+    vim.schedule(function()
+        Snacks.terminal()
+    end)
+end
+
 return {
     "folke/snacks.nvim",
     priority = 1000,
@@ -105,8 +116,10 @@ return {
         { "<leader>gB",      function() Snacks.gitbrowse() end,                                      desc = "Git Browse",               mode = { "n", "v" } },
         { "<leader>gg",      function() Snacks.lazygit() end,                                        desc = "Lazygit" },
         { "<leader>un",      function() Snacks.notifier.hide() end,                                  desc = "Dismiss All Notifications" },
-        { "<c-/>",           function() Snacks.terminal() end,                                       desc = "Toggle Terminal" },
-        { "<c-_>",           function() Snacks.terminal() end,                                       desc = "which_key_ignore" },
+        { "<c-/>",           toggle_terminal,                                                        desc = "Toggle Terminal" },
+        { "<c-_>",           toggle_terminal,                                                        desc = "which_key_ignore" },
+        { "<c-/>",           toggle_terminal_from_terminal,                                          desc = "Toggle Terminal",          mode = "t" },
+        { "<c-_>",           toggle_terminal_from_terminal,                                          desc = "which_key_ignore",        mode = "t" },
         { "]]",              function() Snacks.words.jump(vim.v.count1) end,                         desc = "Next Reference",           mode = { "n", "t" } },
         { "[[",              function() Snacks.words.jump(-vim.v.count1) end,                        desc = "Prev Reference",           mode = { "n", "t" } },
         {
